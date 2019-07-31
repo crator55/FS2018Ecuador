d2b3495b442ad9340ebabc2149e04efab71d9374
Diego <raptor100500@hotmail.com>
2019-07-31 16:11:06 -0500

    Utils.php updated (Added a new instance

--- Lib/Utils.php
+++ Lib/Utils.php
@@ -3,6 +3,7 @@
 namespace FacturaScripts\Plugins\Ecuador\Lib;
 
 
+
 use FacturaScripts\Dinamic\Model\Impuesto;
 use FacturaScripts\Dinamic\Model\Provincia;
 use FacturaScripts\Dinamic\Lib\Import\CSVImport;
@@ -17,51 +18,54 @@ class Utils
     public static function ChangeDefaultTax()
     {
 
-            $impuesto = new Impuesto();
-            $setting_model = new AppSettings();
-            $setting_model->set('default', 'codimpuesto', 'NONE');
+        $impuesto = new Impuesto();
+        $setting_model = new AppSettings();
+        $setting_model->set('default', 'codimpuesto', 'NONE');
 
-            foreach ($impuesto->all() as $value) {
-                $value->delete();
-            }
+        foreach ($impuesto->all() as $value) {
+            $value->delete();
+        }
 
-            $database = new DataBase();
-            $sql = CSVImport::importTableSQL('impuestos');
-            if ($database->exec($sql)) {
-                $setting_model->set('default', 'codimpuesto', 'IVA12');
-                $setting_model->save();
-            }
+        $database = new DataBase();
+        $sql = CSVImport::importTableSQL('impuestos');
+        if ($database->exec($sql)) {
+            $setting_model->set('default', 'codimpuesto', 'IVA12');
+            $setting_model->save();
+        }
 
 
     }
 
     public static function ChangeIdentifer()
     {
-           $identifer = new IdentificadorFiscal();
-            foreach ($identifer->all() as $value) {
-                $value->delete();
-            }
-            $identifer->tipoidfiscal = 'CI';
-            $identifer->save();
+        $identifer = new IdentificadorFiscal();
+        foreach ($identifer->all() as $value) {
+            $value->delete();
+        }
 
-            $identifer->tipoidfiscal = 'RUC';
-            $identifer->save();
+        $tipofiscal = new IdentificadorFiscal();
 
-            $identifer->tipoidfiscal = 'Pasaporte';
-            $identifer->save();
+        $tipofiscal->tipoidfiscal = 'CI';
+        $tipofiscal->save();
+
+        $tipofiscal->tipoidfiscal = 'RUC';
+        $tipofiscal->save();
+
+        $tipofiscal->tipoidfiscal = 'Pasaporte';
+        $tipofiscal->save();
 
     }
 
     public static function ChangeState()
 
     {
-            $state = new Provincia();
-            foreach ($state->all() as $value) {
-                $value->delete();
-            }
-            $database = new DataBase();
-            $sql = CSVImport::importTableSQL('provincias');
-            $database->exec($sql);
+        $state = new Provincia();
+        foreach ($state->all() as $value) {
+            $value->delete();
+        }
+        $database = new DataBase();
+        $sql = CSVImport::importTableSQL('provincias');
+        $database->exec($sql);
 
     }
 }
